ee5d766fba368cb3632bd3789acc774d70fa4958
venkat2602 <19h61a05j4@cvsr.ac.in>
2023-09-18 00:56:12 +0530

    some stylings

--- src/Components/CurrentStatus.js
+++ src/Components/CurrentStatus.js
@@ -5,19 +5,21 @@ const CurrentStatus = ({ current, forecast, location }) => {
   const { name, region, country } = location;
   const { temp_c, temp_f, wind_kph, condition, humidity } = current;
   return (
-    <div className='sm:w-[60vw] h-min mx-auto w-full flex flex-col text-white gap-10'>
-      <div className='h-1/2 flex justify-around items-center flex-wrap gap-1'>
-        <div className='flex flex-col text-center'>
+    <div className='sm:w-[60vw] h-min mx-auto w-full flex flex-col text-white gap-7'>
+      <div className='h-1/2 flex justify-evenly items-center flex-wrap'>
+        <div className='flex flex-row text-center justify-center items-center gap-2'>
           <img
             src={condition.icon}
             alt='condition'
-            className='w-20 h-20 rounded-full'></img>
-          {/* <p className='text-base font-normal opacity-70'>{condition.text}</p> */}
+            className='w-30 h-30 rounded-full'></img>
+          <p className='text-base font-normal opacity-70'>{condition.text}</p>
         </div>
-        <div className='flex flex-col text-center'>
+        <div className='flex flex-row text-center justify-center items-center gap-2'>
           <h1 className='text-2xl font-medium text-center'>{name}</h1>
-          <p className='text-base font-normal opacity-70'>{country}</p>
+          <p className='text-lg font-normal opacity-70'>, {country}</p>
         </div>
+      </div>
+      <div className='flex justify-evenly items-center flex-wrap'>
         <div className='flex flex-col text-center'>
           <h1 className='text-2xl font-medium'>
             {temp_c}°<span className='text-sm font-medium'>C</span>
