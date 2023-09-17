64da95f3cd5c4b280cd3fd55adf67ccc1c153c7a
venkat2602 <19h61a05j4@cvsr.ac.in>
2023-09-17 23:39:46 +0530

    sad2

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -1,6 +1,6 @@
 // const API_KEY = "c5b7bbc4740d403881082212231609";
 export const BASE_URL =
-  "https://api.weatherapi.com/v1/search.json?key=c5b7bbc4740d403881082212231609";
+  "https://api.weatherapi.com/v1/search.json?key=c5b7bbc4740d403881082212231609&";
 
 export const FORECAST_URL =
   "https://api.weatherapi.com/v1/forecast.json?key=c5b7bbc4740d403881082212231609&";
